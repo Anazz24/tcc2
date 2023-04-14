9117d0ba779d247d190507dbe0a8dcc4dc4f810f
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-04-14 08:58:19 -0300

    a

--- src/app/anotacoes/anotacoes.page.ts
+++ src/app/anotacoes/anotacoes.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../services/storage.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TexAnot } from '../models/TexAnot';
 
 @Component({
   selector: 'app-anotacoes',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnotacoesPage implements OnInit {
 
-  constructor() { }
+  formAnotacoes: FormGroup;
+  texAnot: TexAnot = new TexAnot();
+
+  mensagens = {
+    titulo: [
+      { tipo: 'required', mensagem: 'Dê um Título para sua anotação.'},
+    ],
+    texto: [
+      { tipo: 'required', mensagem: 'Escreva uma Anotação.'},
+    ],
+  };
+
+  constructor(private formBuilder: FormBuilder, private storageService: StorageService, private route: Router) {
+    this.formAnotacoes = this.formBuilder.group({
+      titulo: ['', Validators.compose([Validators.required])],
+      texto: ['', Validators.compose([Validators.required])],
+    });
+   }
 
   ngOnInit() {
   }
+
+  async salvarAnotacao() {
+    if(this.formAnotacoes.valid){
+      this.texAnot.titulo = this.formAnotacoes.value.titulo;
+----------------------------------------------------
+      this.route.navigateByUrl('/tabs/tab1');
+    }else{
+      alert('Formulário Inválido!');
+    }
+    }
 
 }
